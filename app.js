a23b3b9ce72b27c4da327503777376c9467f76be
Márcio <marcioadrianoalvesjunior@gmail.com>
2021-02-21 16:24:46 -0300

    Intro Animation done

--- app.js
+++ app.js
@@ -7,3 +7,16 @@
             .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
             .join('')
     })
+//Intro animation
+    const  
+        outsideLogo = document.querySelector('nav a img'),
+        insideLogo = document.querySelector('.main.info .content-wrapper img.logo '),
+        lefttitle = document.querySelector('h1.left-text'),
+        leftText = document.querySelector('.main.info .content-wrapper p'),
+        card = document.querySelector('.card'),
+    items = [outsideLogo,insideLogo,leftText, lefttitle,card]
+
+    items.forEach(item =>{
+        item.style.opacity = "1"
+        item.style.transform = "translate(0,0)"
+    })
