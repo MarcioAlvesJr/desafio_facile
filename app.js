23fbf3419af1ed90d1d1a882e06888d51925170c
Márcio <marcioadrianoalvesjunior@gmail.com>
2021-02-21 13:13:24 -0300

    Responsive CSS

---
+++ app.js
@@ -0,0 +1,12 @@
+
+//Form wave effect
+    const labels = document.querySelectorAll(".form-control label")
+    labels.forEach(label => {
+        label.innerHTML = label.innerText
+            .split('')
+            .map((letter, idx) => `<span style="transition-delay:${idx * 50}ms">${letter}</span>`)
+            .join('')
+    })
+
+//Fixed 
+
